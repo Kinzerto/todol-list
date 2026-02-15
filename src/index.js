a04343fcac2f6b58d1a888041b90fa7bfa528c0f
Gwyneth Device <enriquekinth@gmail.com>
2026-02-16 05:58:58 +0800

    Implement project creation and task management functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import { greetings } from "./welcome.js";
-import "./style.scss";
+import { Project } from "./scripts/list.js";
+import { AddTask } from "./scripts/list.js";
+import { createElement } from "./utils/createElement.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.textContent = greetings;
-})  
+const projects = document.querySelector('.projects');
+const tasks = document.querySelector('.tasks');
+const createProject = document.querySelector('button');
+
+
+createProject.addEventListener('click', (e) => {
+    const inp = prompt();
+    if(!inp) return;
+    const newProject = new Project(inp);
+    addFunctionality(newProject);
+});
+
+function addFunctionality (data) {
+
+    const DOMButtons = createElement('button',data.name,data.name, projects)
+    DOMButtons.addEventListener('click', (e) => {
+        const inp = prompt();
+        data.addTask(inp)
+        console.log(data.showList);
+        console.log(Project.allProjects);
+    })
+
+    
+}
+
+
+
+
+
+
