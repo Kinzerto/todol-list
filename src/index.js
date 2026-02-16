2f60d9e50938c62ba80bf027ab0ecd1a2f31f1f5
Gwyneth Device <enriquekinth@gmail.com>
2026-02-16 22:33:01 +0800

    Refactor project creation and task management functionality

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-import { Project } from "./scripts/list.js";
-import { AddTask } from "./scripts/list.js";
+import { Project, createProject, AddTask} from "./scripts/list.js";
 import { createElement } from "./utils/createElement.js";
+
 
 const projects = document.querySelector('.projects');
 const tasks = document.querySelector('.tasks');
-const createProject = document.querySelector('button');
+const projectButton = document.querySelector('button');
 
 
-createProject.addEventListener('click', (e) => {
-    const inp = prompt();
-    if(!inp) return;
-    const newProject = new Project(inp);
-    addFunctionality(newProject);
-});
-
-function addFunctionality (data) {
-
-    const DOMButtons = createElement('button',data.name,data.name, projects)
-    DOMButtons.addEventListener('click', (e) => {
-        const inp = prompt();
-        data.addTask(inp)
-        console.log(data.showList);
-        console.log(Project.allProjects);
-    })
-
-    
-}
+createProject(projectButton);
 
 
 
--- src/scripts/list.js
+++ src/scripts/list.js
@@ -1,8 +1,10 @@
+import { createElement } from "../utils/createElement.js";
+
 export class Project {
     static #allProjects = {};
-    #list = {};
-    constructor(name = 'default') {
-        this.#list[name] = [];
+    #list = [];
+    constructor(name) {
+        if(!name || Project.#allProjects[name]) return;
         this.name = name;
         Project.#allProjects[name] = this.#list;
     }
@@ -12,17 +14,16 @@ export class Project {
     }
 
     addTask(task) {
-        if(!task) return;
-        this.#list[this.name].push(task);
+        if (!task) return;
+        this.#list.push(task);
     }
 
     removeTask(taskId) {
         this.#list[this.name] = this.#list[this.name].filter(task => task.id !== taskId);
     }
 
-    
-
     get showList() {
+
         return structuredClone(this.#list);
     }
 
@@ -32,7 +33,7 @@ export class Project {
 }
 
 export class AddTask {
-    constructor(title, description, dueDate, priority, notes, checklist) {
+    constructor(title, description = '', dueDate = '', priority = '', notes = '', checklist = '') {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
@@ -44,12 +45,40 @@ export class AddTask {
     }
 }
 
-// const task1 = new Project('Home Task');
-// const task2 = new Project('School Task');
 
-// task1.addTask(new AddTask('Clean Room', 'Clean the entire room', '2024-07-01', 'High', 'Use the new cleaning supplies', ['Vacuum', 'Dust', 'Mop']), 'Home Task');
-// task1.addTask(new AddTask('Grocery Shopping', 'Buy groceries for the week', '2024-07-03', 'Medium', 'Make a list of needed items', ['Milk', 'Bread', 'Eggs']), 'Home Task');
-// task2.addTask(new AddTask('Finish Homework', 'Complete all math problems', '2024-07-02', 'Medium', 'Focus on the difficult problems first', ['Problem 1', 'Problem 2', 'Problem 3']), 'School Task');
+export function createProject(element) {
+    element.addEventListener('click', (e) => {
+        const inp = prompt();
+        if (!inp) return;
+        const newProject = new Project(inp);
+        addFunctionality(newProject);
+    });
+}
+const projectsContainer = document.querySelector('.projects')
+const tasks = document.querySelector('.tasks');
 
-// console.log(task1.showList[0]);
+function addFunctionality(data) {
+    const DOMButtons = createElement('button', data.name, data.name, projectsContainer)
+    DOMButtons.addEventListener('click', (e) => {
+        const inp = prompt();
+        data.addTask(inp)
+        displayTasks(data.showList);
+        // console.log(data.showList);
+
+        console.log(Project.allProjects);
+    })
+}
+
+
+function displayTasks(array) {
+    tasks.replaceChildren();
+    array.forEach(element => {
+        const task = createElement('div', '', element, tasks)
+        task.addEventListener('click', () => {
+            console.log(array.indexOf(element));
+        })
+    });
+
+}
+
 
