63326f6cdadd593b395bc4afba407457f464f750
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-03-04 11:12:29 +0800

    Refactor task management in Project class and update display logic in list.js

--- src/scripts/list.js
+++ src/scripts/list.js
@@ -19,11 +19,18 @@ export class Project {
     }
 
     removeTask(taskId) {
-        this.#list[this.name] = this.#list[this.name].filter(task => task.id !== taskId);
+        this.#list = this.#list.filter((task) => task.id !== taskId);
+        Project.#allProjects[this.name] = this.#list;
+    }
+
+    toggleTaskCompletion(taskId) {
+        const task = this.#list.find((task) => task.id === taskId);
+        if (task) {
+            task.completed = !task.completed;
+        }
     }
 
     get showList() {
-
         return structuredClone(this.#list);
     }
 
@@ -45,10 +52,12 @@ export class AddTask {
     }
 }
 
+// DOM Elements
 const content = document.querySelector('.content');
 const addedProjects = document.querySelector('.addedProjects');
 const tasks = document.querySelector('.tasks');
-// tasks.textContent = 'mdwdomwd';
+const addButton = document.querySelector('.addButton');
+
 
 export function createProject(element) {
     element.addEventListener('click', (e) => {
@@ -60,39 +69,43 @@ export function createProject(element) {
 }
 
 function displayProject(data) {
-    
+
     const DOMButtons = createElement('button', 'project', '', addedProjects);
     const spanHash = createElement('span', 'hash', '', DOMButtons);
     DOMButtons.append(data.name);
 
     DOMButtons.addEventListener('click', (e) => {
-        // const inp = prompt();
-        // data.addTask(inp);
-
-        
-        displayTasks(data.showList);
-        console.log(Project.allProjects);
-    })
+        displayTasks(data);
+    });
 }
 
 
 function displayTasks(array) {
     tasks.replaceChildren();
+    addButton.replaceChildren();
+    console.log(array);
     //adds a button to add tasks to the project
-    createElement('button', 'addTaskBtn', 'Add Task', content).addEventListener('click', () => {
+    createElement('button', 'addTaskBtn', 'Add Task', addButton).addEventListener('click', () => {
         const title = prompt('Task Title');
-        
+
         if (!title) return;
         const newTask = new AddTask(title);
-        array.push(newTask);
+        array.addTask(newTask);
         console.log(array);
         displayTasks(array);
-        
+
     });
-    array.forEach(element => {
+
+
+    console.log(array.showList);
+    array.showList.forEach((element, index) => {
         const task = createElement('div', '', element.title, tasks);
         task.addEventListener('click', () => {
-            console.log(array.indexOf(element));
+
+            array.toggleTaskCompletion(element.id);
+            console.log(Project.allProjects);
+            displayTasks(array);
+
         });
     });
 
