69ff1758c5c4c80751eb5d352a859d816aa3bef4
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-04-11 04:38:47 +0800

    Refactor task filtering; streamline filterTask function by removing unused imports and simplifying project filtering logic.

--- src/controllers/filterTaskController.js
+++ src/controllers/filterTaskController.js
@@ -1,9 +1,9 @@
+import emptyState from '../assets/images/folder.png'
 import { tasks, addButton, headerTitle } from '../index.js';
 import { Project } from '../models/Project.js';
 import { state } from '../state.js';
-import { createElement, findProjectNameByTaskId } from '../utils/tools.js';
-import { renderTasks } from '../viewer/renderTasks.js';
-import { isToday } from "date-fns";
+import { renderFilteredTask } from '../viewer/renderFilteredTask.js';
+import { compareAsc, isToday } from "date-fns";
 
 
 export function filterTask() {
@@ -13,16 +13,15 @@ export function filterTask() {
     addButton.replaceChildren();
 
     const projects = Project.allProjects;
-
+    let filtered = null
     for (const [projectName, project] of Object.entries(projects)) {
-        let filtered = null
 
         // ✅ filter ONLY this project's completed tasks
         if (state.currentView === 'completed') {
             filtered = project.showList.filter(task => task.completed);
         } else if (state.currentView === 'important') {
             filtered = project.showList.filter(task => task.priority === 'high' && !task.completed);
-        } else if (state.currentView === 'allTasks') {
+        } else if (state.currentView === 'home') {
             filtered = project.showList.filter(task => !task.completed);
         } else if (state.currentView === 'today') {
             filtered = project.showList.filter((task) => {
@@ -30,39 +29,14 @@ export function filterTask() {
                 return !task.completed && task.dueDate && isToday(new Date(task.dueDate));
             });
         } else if (state.currentView === 'upcoming') {
-            filtered = project.showList.filter((task) => {
-                // console.log(task.due);
-                return !task.completed && task.dueDate && !isToday(new Date(task.dueDate));
-            });
+            filtered = project.showList
+                .filter(task => !task.completed && task.dueDate && !isToday(new Date(task.dueDate)))
+                .sort((a, b) => compareAsc(new Date(a.dueDate), new Date(b.dueDate)));
         }
 
         // ❗ skip empty projects
         if (filtered.length === 0) continue;
-
-        // ✅ create section per project
-        const section = createElement('div', 'projectSection', '', tasks);
-        section.dataset.project = projectName; // for potential future use
-
-
-        // ✅ render tasks INSIDE this section
-        renderTasks(filtered, section, true);
-        const projectHeader = createElement('div', 'projectHeader', '', section);
-
-        const projectNameSpan = createElement('span', 'projectNameSpan', projectName, projectHeader);
-        section.prepend(projectHeader);
-
-
-        const collapse = document.createElement('span');
-        collapse.classList.add('material-symbols-outlined', 'collapse');
-        collapse.textContent = 'keyboard_arrow_down';
-        projectHeader.prepend(collapse);
-        // const collapse = createElement('span', 'collapse', '▼', projectHeader);
-        // section.classList.add('collapsed');
-        collapse.addEventListener('click', (e) => {
-            collapse.textContent = collapse.textContent === 'keyboard_arrow_down' ? 'keyboard_arrow_right' : 'keyboard_arrow_down';
-            e.stopPropagation();
-            section.classList.toggle('collapsed');
-
-        });
+        renderFilteredTask(projectName, filtered);
     }
+
 }
